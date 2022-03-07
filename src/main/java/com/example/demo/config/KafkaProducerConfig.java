40c55d2531e170d3a2d71ffc94b2d86406f1a673
eric soucy <eric.soucy@gmail.com>
2022-03-06 20:37:24 -0500

    kafka producer setup  and send message via commandlineerunner

--- src/main/java/com/example/demo/config/KafkaProducerConfig.java
+++ src/main/java/com/example/demo/config/KafkaProducerConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,11 +20,23 @@ public class KafkaProducerConfig {
   @Value("${spring.kafka.bootstrap-servers}")
   private String boostrapServers;
 
+  @Value("${spring.kafka.properties.sasl.jaas.config}")
+  private String saslJassConfig;
+
+  @Value("${spring.kafka.properties.sasl.mechanism}")
+  private String saslMechanism;
+
+  @Value("${spring.kafka.properties.security.protocol}")
+  private String securityProtocol;
+
   public Map<String, Object> producerConfig() {
     Map<String, Object> props = new HashMap<>();
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+    props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+    props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJassConfig);
     return props;
   }
 
